69a4b4d08dfc8a28c91961cb810c976b8938b3a6
Mike DeVita <mike@devita.co>
2015-05-26 21:07:01 -0700

    moving the API to JSON only, stripping the custom actions.
    This is part of the move to angular and a full blown "REST" api.

--- api/controllers/ItemTypeController.js
+++ api/controllers/ItemTypeController.js
@@ -6,21 +6,6 @@
  */
 
 module.exports = {
-	index: function index(req, res){
-
-		Itemtype.find()
-		.populate('items')
-		.exec(function list(err, itemtypes){
-			if(err) return res.send(500);
-
-			if(req.wantsJSON) {
-				res.json(itemtypes);
-			}else{
-				res.view('itemtype/index', { itemtypes: itemtypes});
-			}
-
-		});
-	},
 
 	findByName: function findByName(req, res){
 
@@ -38,13 +23,5 @@ module.exports = {
 			}
 
 		});
-	},
-
-	create: function create(req, res){
-		res.view('itemtype/create');
-	},
-
-	createNew: function createNew(req, res){
-		res.send(200);
-	},
+	}
 };
